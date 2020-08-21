69a6b7a52b8c85718479abd994aa8e89d2031f56
nasum <tomato.wonder.life@gmail.com>
2020-08-21 18:58:27 +0900

    fix tag list

--- docs/.vuepress/theme/layouts/Tag.vue
+++ docs/.vuepress/theme/layouts/Tag.vue
@@ -1,19 +1,38 @@
 <template>
   <div class="l-main">
     <ul class="c-tag-list">
-      <li class="c-tag-list__tag" v-for="tag in $tag.list">
-        <router-link class="page-link" :to="tag.path">{{
-          tag.name
-        }}</router-link>
+      <li class="c-tag-list__tag" v-for="tag in tags">
+        <router-link class="page-link" :to="tag.path">{{tag.name}}: {{tag.pages.length}}</router-link>
       </li>
     </ul>
   </div>
 </template>
 
+<script>
+export default {
+  name: "Tag",
+  computed: {
+    tags() {
+      const tagList = this.$tag.list;
+      return tagList.sort((tagA, tagB) => {
+        if (tagA.pages.length < tagB.pages.length) {
+          return 1;
+        } else if (tagA.pages.length > tagB.pages.length) {
+          return -1;
+        } else {
+          return 0;
+        }
+      });
+    },
+  },
+};
+</script>
+
 <style lang="scss">
 .c-tag-list {
   display: flex;
   flex-direction: row;
+  flex-wrap: wrap;
   list-style: none;
   padding-bottom: 6px;
 
@@ -22,6 +41,7 @@
     border: 1px solid #d8dadb;
     border-radius: 5px;
     margin-right: 4px;
+    margin-bottom: 4px;
     a {
       color: #222;
       text-decoration: none;
