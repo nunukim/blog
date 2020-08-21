0006211fa87bc7167297c6d8e964f7f4c90480a4
nasum <tomato.wonder.life@gmail.com>
2020-08-21 17:54:45 +0900

    add tag for article

--- docs/.vuepress/theme/layouts/Post.vue
+++ docs/.vuepress/theme/layouts/Post.vue
@@ -1,9 +1,37 @@
 <template>
-  <Content class="c-article-main" />
+  <div class="c-article-main">
+    <ul class="c-tag-list">
+      <li class="c-tag-list__tag" v-for="tag in $frontmatter.tag">
+        <router-link class="page-link" :to="`/tags/${tag}/`">
+          {{
+          tag
+          }}
+        </router-link>
+      </li>
+    </ul>
+    <Content />
+  </div>
 </template>
 
 <style src="prism-themes/themes/prism-dracula.css"></style>
 <style lang="scss">
+.c-tag-list {
+  display: flex;
+  flex-direction: row;
+  list-style: none;
+  padding-bottom: 6px;
+
+  &__tag {
+    padding: 4px;
+    border: 1px solid #d8dadb;
+    border-radius: 5px;
+    margin-right: 4px;
+    a {
+      color: #222;
+      text-decoration: none;
+    }
+  }
+}
 .c-article-main {
   max-width: 1024px;
   width: 100%;
