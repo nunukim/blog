0253613b06a378f13533ec751f3f1a997f0b6a37
nasum <tomato.wonder.life@gmail.com>
2021-01-04 18:21:48 +0900

    update-webpack-v5-webpack-manifest-plugin-v3

--- docs/.vuepress/theme/layouts/Post.vue
+++ docs/.vuepress/theme/layouts/Post.vue
@@ -3,9 +3,7 @@
     <ul class="c-tag-list">
       <li class="c-tag-list__tag" v-for="tag in $frontmatter.tag">
         <router-link class="page-link" :to="`/tags/${tag}/`">
-          {{
-          tag
-          }}
+          {{ tag }}
         </router-link>
       </li>
     </ul>
@@ -47,7 +45,8 @@
     margin: 10px 0;
   }
   p {
-    line-height: 30px;
+    line-height: 20px;
+    padding: 10px 0;
   }
   ol {
     margin-left: 48px;
@@ -60,6 +59,40 @@
     width: auto;
     border: 1px solid #282a36;
     border-radius: 0.3rem;
+  }
+
+  table {
+    padding: 0;
+  }
+  table tr {
+    border-top: 1px solid #cccccc;
+    background-color: white;
+    margin: 0;
+    padding: 0;
+  }
+  table tr:nth-child(2n) {
+    background-color: #f8f8f8;
+  }
+  table tr th {
+    font-weight: bold;
+    border: 1px solid #cccccc;
+    text-align: left;
+    margin: 0;
+    padding: 6px 13px;
+  }
+  table tr td {
+    border: 1px solid #cccccc;
+    text-align: left;
+    margin: 0;
+    padding: 6px 13px;
+  }
+  table tr th :first-child,
+  table tr td :first-child {
+    margin-top: 0;
+  }
+  table tr th :last-child,
+  table tr td :last-child {
+    margin-bottom: 0;
   }
 }
 </style>
